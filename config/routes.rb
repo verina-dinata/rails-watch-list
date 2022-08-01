6feef5a9599d4960808f0b2a8a169f58d6e962ac
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-01 21:50:45 +0800

    Add lists and bookmarks controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # read all
+  # get 'lists', to: 'lists#index'
+
+  # # create a new list
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+
+  # # read specific list
+  # get 'lists/:id', to: 'lists#show'
+
+  resources :lists, only: %i(index show new create) do
+    resources :bookmarks, only: %i(new create)
+  end
+
+  resources :bookmarks, only: %i(destroy)
 end
