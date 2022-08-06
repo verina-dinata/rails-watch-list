95a54349bec34ab419dcf5da40e5e56a25824978
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-06 15:00:01 +0800

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "lists#index"
+  root to: "lists#index"
 
   # read all
   # get 'lists', to: 'lists#index'
