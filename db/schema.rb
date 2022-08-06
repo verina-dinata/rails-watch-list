8997ab9a5b6c63b12f19555dcf8e40af811a36f1
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-06 13:32:17 +0800

    update gem bundle

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_124331) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
-    t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
   end
 
   create_table "movies", force: :cascade do |t|
